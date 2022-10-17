e60e3ef30266cfb8f1f2eb21e84f2736e3a35860
Moros1138 <moros1138@gmail.com>
2022-10-16 20:08:51 -0400

    add default/no wasm player view

---
+++ resources/views/player-intro.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>No Program To Run</title>
+    <style>
+        html,
+        body {
+            font-family: "Droid Sans Mono", "monospace", monospace;
+            font-weight: normal;
+            font-size: 14px;
+            margin: 0;
+            padding: 1rem;
+        }
+
+        body {
+            background: #222;
+            color: #ded;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Welcome to PGEtinker</h1>
+    <p>Inspiring Quote? Instructions? Who needs 'em!</p>
+    <p>Let's just jump into some code and read the docs later!</p>
+    <p>What do you mean? There's no docs?.... Hmmm interesting.</p>
+</body>
+</html>
+       
