e150df9b9c184d14df4fbb509e71ae941fab0bf6
Moros1138 <moros1138@gmail.com>
2022-10-19 17:34:44 -0400

    major refactor and clean up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,293 +1,298 @@
 import './bootstrap';
 import * as monaco from 'monaco-editor';
+import axios from 'axios';
 
-// retrieve the default code value
-let defaultValue = $('#defaultCode').text();
-$('#defaultCode').remove();
+let PGEtinker = function(){
+    
+    // shim String to convert html entities
+    String.prototype.toHtmlEntities = function() { return this.replace(/./gm, function(s) { return (s.match(/[a-z0-9\s]+/i)) ? s : "&#" + s.charCodeAt(0) + ";"; }); };
+    
+    // retrieve the default code value
+    let defaultValue = $('#defaultCode').text();
+    $('#defaultCode').remove();
 
-// Editor Panel
-window.monEditor = null;
-window.monDecorator = null;
+    // Editor Panel
+    let monaco_Editor = null;
+    let monaco_EditorDecorator = null;
+    let elem_Editor = null;
 
-// Information Panel
-window.monInfo = null;
-let elemInfo  = null;
-let regExInfo = /(source.cpp|olcPixelGameEngine.h):([0-9]+):([0-9]+): ([a-zA-Z0-9]+):/g; 
+    // Information Panel
+    let elem_Information   = null;
+    let regExInfo = /(source.cpp|olcPixelGameEngine.h):([0-9]+):([0-9]+): ([a-zA-Z0-9]+):/g;     
+    
+    // Console Panel
+    let elem_Console = null;
 
-// Console Panel
-let elemConsole = null;
+    // Player Panel
+    let elem_PlayerFrame  = null;
+    let elem_PlayerStatus = null;
 
-// Player Panel
-let elemPlayerIframe = null;
-
-String.prototype.toHtmlEntities = function() {
-    return this.replace(/./gm, function(s) {
-        // return "&#" + s.charCodeAt(0) + ";";
-        return (s.match(/[a-z0-9\s]+/i)) ? s : "&#" + s.charCodeAt(0) + ";";
-    });
-};
-
-
-let pgeTinkerSavedLayout = window.localStorage.getItem('pgeTinkerSavedLayout');
-let layoutConfig = null;
-
-if(pgeTinkerSavedLayout === null)
-{
-    // set the default layout
-    layoutConfig = {
-        settings: {
-            showPopoutIcon: false,
-        },
-        content: [{
-            type: 'column',
+    /*************************************************************************
+     * LAYOUT CONFIGURATION
+     *************************************************************************/
+    if(!localStorage.getItem('pgeTinkerSavedLayout'))
+    {
+        localStorage.setItem('pgeTinkerSavedLayout', JSON.stringify({
+            settings: {
+                showPopoutIcon: false,
+            },
             content: [{
-                type: 'row',
+                type: 'column',
                 content: [{
-                    type: 'component',
-                    componentName: 'editor',
-                    componentState: { label: 'Editor Panel' },
-                    isClosable: false,
+                    type: 'row',
+                    content: [{
+                        type: 'component',
+                        componentName: 'editor',
+                        componentState: { label: 'Editor Panel' },
+                        isClosable: false,
+                    },{
+                        type: 'component',
+                        componentName: 'player',
+                        componentState: { label: 'Player Panel' },
+                        isClosable: false,
+                    }]
                 },{
-                    type: 'component',
-                    componentName: 'player',
-                    componentState: { label: 'Player Panel' },
-                    isClosable: false,
-                }]
-            },{
-                type: 'stack',
-                height: 15,
-                activeItemIndex: 1,
-                content: [{
-                    type: 'component',
-                    componentName: 'info',
-                    componentState: { label: 'Info Panel' },
-                    isClosable: false,
-                },{
-                    type: 'component',
-                    componentName: 'console',
-                    componentState: { label: 'Console Panel' },
-                    isClosable: false,
+                    type: 'stack',
+                    height: 15,
+                    activeItemIndex: 1,
+                    content: [{
+                        type: 'component',
+                        componentName: 'info',
+                        componentState: { label: 'Info Panel' },
+                        isClosable: false,
+                    },{
+                        type: 'component',
+                        componentName: 'console',
+                        componentState: { label: 'Console Panel' },
+                        isClosable: false,
+                    }]
                 }]
             }]
-        }]
-    };
-}
-else
-{
-    layoutConfig = JSON.parse(pgeTinkerSavedLayout);
-}
+        })); // setItem(JSON.stringify(stuff))
 
-// Golden Layout
-let pgeLayout = new GoldenLayout(layoutConfig, $('#content'));
-
-window.Share = function()
-{
-    alert('not implemented');
-}
-
-// Reset Layout
-window.ResetLayout = function()
-{
-    window.localStorage.removeItem('pgeTinkerSavedLayout');
-    window.location.reload();
-}
-
-// Compile Code
-window.Compile = function()
-{
-    let status = document.querySelector('#player-panel div');
+    }
     
-    status.className = 'compiling';
-    elemInfo.innerHTML = '';
+    // Golden Layout
+    let goldenLayout_PGEtinker = new GoldenLayout(JSON.parse(localStorage.getItem('pgeTinkerSavedLayout')), $('#content'));
+
+
+    /*************************************************************************
+     * LAYOUT COMPONENTS
+     *************************************************************************/
     
-    window.monDecorator = monEditor.deltaDecorations(monDecorator, []);
-    
-    axios.post('/api/compile', {code: monEditor.getModel().getValue()})
-    .then(function(response)
+    // Component: console panel
+    goldenLayout_PGEtinker.registerComponent( 'console', function( container, componentState )
     {
-        if(response.data.success)
-        {
-            elemPlayerIframe.src = '/player';
-            setTimeout(function() { status.className = ''; }, 1000);
-        }
-        else
-        {
-            status.className = 'fail';
-        }
-
-        let info = new Array();
-        let m    = null;
-        
-        while((m = regExInfo.exec(response.data.messages)) !== null)
-        {
-            if (m.index === regExInfo.lastIndex)
-            {
-                re.lastIndex++;
-            }
-
-            info.push({ input: m[0], line: parseInt(m[2]), column: parseInt(m[3]), type: m[4] });
-        }
-        
-        let out = response.data.messages;
-        var entries = [];
-        
-        info.forEach(function(item)
-        {
-
-            if(item.input.indexOf('source.cpp') === 0)
-            {
-                out = out.replace(item.input, `<a href="#" onclick="monEditor.setPosition({ lineNumber: ${item.line}, column: ${item.column} }); monEditor.revealPositionInCenter({ lineNumber: ${item.line}, column: ${item.column} }); monEditor.focus(); return false;">${item.input}</a>`);
-                
-                // add the decoration
-                entries.push({
-                    range: new monaco.Range(item.line, 1, item.line, 2),
-                    options: {
-                        isWholeLine: true,
-                        className: 'editor-'+item.type,
-                    }
-                });
-                
-            }
-                
-            if(item.input.indexOf('olcPixelGameEngine.h') === 0)
-                out = out.replace(item.input, `<a href="https://github.com/OneLoneCoder/olcPixelGameEngine/blob/develop/olcPixelGameEngine.h#L${item.line}" target="_blank">${item.input}</a>`);
-        });
-        
-        elemInfo.innerHTML = out;
-        window.monDecorator = monEditor.deltaDecorations([], entries);
-    })
-    .catch(function(error)
-    {
-        console.log(error);
+        container.getElement().html( '<div id="console-panel"><div></div></div>' );
     });
 
-}
-
-// Refresh Player
-window.RefreshPlayer = function()
-{
-    let status = document.querySelector('#player-panel div');
-    status.className = 'loading';
-    
-    elemInfo.innerHTML = '';
-    elemPlayerIframe.src = '/player';
-    setTimeout(function() { status.className = ''; }, 1000);
-}
-
-// editor component
-pgeLayout.registerComponent( 'editor', function( container, componentState )
-{
-    container.getElement().html(`
-<div id="editor-panel">
-    <div class="menu">
-        <ul>
-            <li><button type="button" onclick="Share(); return false;">Share</button></li>
-            <li class="separator"></li>
-            <li><button type="button" onclick="ResetLayout(); return false;">Reset Layout</button></li>
-            <li><button type="button" onclick="Compile(); return false;">Build &amp; Run</button></li>
-            <li><button type="button" onclick="RefreshPlayer(); return false;">Refresh Player</button></li>
-        </ul>
-    </div>
-    <div class="code-editor"></div>
-</div>`);
-    
-    container.on('open', function()
+    // Component: editor panel 
+    goldenLayout_PGEtinker.registerComponent( 'editor', function( container, componentState )
     {
-        let elemCode    = document.querySelector('#editor-panel .code-editor');
-        let elemCompile = container.getElement().find('.compile-button');
+        container.getElement().html(`<div id="editor-panel"><div class="menu"><ul><li><button type="button" onclick="PGEtinker.Share(); return false;">Share</button></li><li class="separator"></li><li><button type="button" onclick="PGEtinker.ResetLayout(); return false;">Reset Layout</button></li><li><button type="button" onclick="PGEtinker.Compile(); return false;">Build &amp; Run</button></li><li><button type="button" onclick="PGEtinker.RefreshPlayer(); return false;">Refresh Player</button></li></ul></div><div class="code-editor"></div></div>`);
+        
+        container.on('resize', function()
+        {
+            if(monaco_Editor !== null)
+                monaco_Editor.layout();
+        });
+    });
 
-        window.monEditor = monaco.editor.create(elemCode, {
+    // Component: information panel
+    goldenLayout_PGEtinker.registerComponent( 'info', function( container, componentState )
+    {
+        container.getElement().html( '<div id="info-panel" data-type="text/css"></div>' );
+    });
+
+    // Component: player panel
+    goldenLayout_PGEtinker.registerComponent( 'player', function( container, componentState )
+    {
+        container.getElement().html( '<div id="player-panel"><iframe sandbox="allow-scripts" src="/player"></iframe><div></div></div>' );
+    });
+
+    /*************************************************************************
+     * LAYOUT INITIALIZAION
+     *************************************************************************/
+    goldenLayout_PGEtinker.on('initialised', function()
+    {
+        // populate elements
+        elem_Editor       = document.querySelector('#editor-panel .code-editor');
+        elem_Information  = document.querySelector('#info-panel');
+        elem_Console      = document.querySelector('#console-panel div');
+        elem_PlayerFrame  = document.querySelector('#player-panel iframe');
+        elem_PlayerStatus = document.querySelector('#player-panel div');
+
+        // create code editor
+        monaco_Editor = monaco.editor.create(elem_Editor, {
             value: defaultValue,
             language: 'cpp',
             theme: 'vs-dark',
         });
 
-        window.monDecorator = monEditor.deltaDecorations([], []);
-
-        container.on('resize', function()
+        // initialize code editor's decorations
+        monaco_EditorDecorator = monaco_Editor.deltaDecorations([], []);
+        
+        // handle resizing
+        window.addEventListener('resize', function(e)
         {
-            monEditor.layout();
+            goldenLayout_PGEtinker.updateSize();
         });
 
-    });
-});
+        // handle postMessage, specifically from the player iframe
+        window.addEventListener('message', function(e)
+        {
+            // guarantee we're processing a message coming from the player frame
+            if(!(e.origin === 'null' && e.source === elem_PlayerFrame.contentWindow))
+                return;
 
-// player component
-pgeLayout.registerComponent( 'player', function( container, componentState )
-{
-    container.getElement().html( '<div id="player-panel"><iframe sandbox="allow-scripts" src="/player"></iframe><div></div></div>' );
-    container.on('open', function()
+            // sanity check, is event set?
+            if(e.data.event === undefined)
+                return;
+            
+            // handle console clearing. 'pgetinker:console-clear' event dispatched from player iframe
+            if(e.data.event === 'pgetinker:console-clear')
+            {
+                elem_Console.innerHTML = '';
+                return;
+            }
+            
+            // handle console writing. 'pgetinker:console-write' event dispatched from player iframe
+            if(e.data.event === 'pgetinker:console-write')
+            {
+                elem_Console.innerHTML += e.data.text.toHtmlEntities() + '<br>';
+                elem_Console.scrollTop = elem_Console.scrollHeight;
+                return;
+            }
+        });
+    });
+
+    // save the state
+    goldenLayout_PGEtinker.on('stateChanged', function()
     {
-        elemPlayerIframe = document.querySelector('#player-panel iframe');
+        var state = JSON.stringify( goldenLayout_PGEtinker.toConfig() );
+        localStorage.setItem( 'pgeTinkerSavedLayout', state );
     });
-});
 
-// info component
-pgeLayout.registerComponent( 'info', function( container, componentState )
-{
-    container.getElement().html( '<div id="info-panel" data-type="text/css"></div>' );
+    // initialize the layout
+    goldenLayout_PGEtinker.init();
 
-    container.on('open', function()
-    {
-        elemInfo = document.querySelector('#info-panel');
-    });
-});
-
-pgeLayout.registerComponent( 'console', function( container, componentState )
-{
-    container.getElement().html( '<div id="console-panel"><div></div></div>' );
+    /*************************************************************************
+     * API FUNCTIONS
+     *************************************************************************/
     
-    container.on('open', function()
+    function Compile()
     {
-        elemConsole = document.querySelector('#console-panel div');
-    });
-});
-
-// stuff that needs to happen when the layout has been initialised.
-pgeLayout.on('initialised', function()
-{
-    window.addEventListener('resize', function(e)
-    {
-        pgeLayout.updateSize();
-    });
-
-    // force empty console
-    elemConsole.innerHTML = '';
-
-    // handle postMessage, specifically from the player iframe
-    window.addEventListener('message', function(e)
-    {
-        // guarantee we're processing a message coming from the player frame
-        if(!(e.origin === 'null' && e.source === elemPlayerIframe.contentWindow))
-            return;
-
-        // sanity check, is event set?
-        if(e.data.event === undefined)
-            return;
+        elem_PlayerStatus.className = 'compiling';
+        elem_Information.innerHTML = '';
         
-        // handle console clearing. 'pgetinker:console-clear' event dispatched from player iframe
-        if(e.data.event === 'pgetinker:console-clear')
-        {
-            elemConsole.innerHTML = '';
-            return;
-        }
+        monaco_EditorDecorator = monaco_Editor.deltaDecorations(monaco_EditorDecorator, []);
         
-        // handle console writing. 'pgetinker:console-write' event dispatched from player iframe
-        if(e.data.event === 'pgetinker:console-write')
+        axios.post('/api/compile', {code: monaco_Editor.getModel().getValue()})
+        .then(function(response)
         {
-            elemConsole.innerHTML += e.data.text.toHtmlEntities() + '<br>';
-            elemConsole.scrollTop = elemConsole.scrollHeight;
-            return;
-        }
-    });
-});
+            if(response.data.success)
+            {
+                elem_PlayerFrame.src = '/player';
+                setTimeout(function() { elem_PlayerStatus.className = ''; }, 1000);
+            }
+            else
+            {
+                elem_PlayerStatus.className = 'fail';
+            }
+    
+            let info = new Array();
+            let m    = null;
+            
+            while((m = regExInfo.exec(response.data.messages)) !== null)
+            {
+                if (m.index === regExInfo.lastIndex)
+                {
+                    re.lastIndex++;
+                }
+    
+                info.push({ input: m[0], line: parseInt(m[2]), column: parseInt(m[3]), type: m[4] });
+            }
+            
+            let out = response.data.messages;
+            var entries = [];
+            
+            info.forEach(function(item)
+            {
+    
+                if(item.input.indexOf('source.cpp') === 0)
+                {
+                    out = out.replace(item.input, `<a href="#" onclick="PGEtinker.GotoAndFocus(${item.line}, ${item.column}); return false;">${item.input}</a>`);
+                    
+                    // add the decoration
+                    entries.push({
+                        range: new monaco.Range(item.line, 1, item.line, 2),
+                        options: {
+                            isWholeLine: true,
+                            className: 'editor-'+item.type,
+                        }
+                    });
+                    
+                }
+                    
+                if(item.input.indexOf('olcPixelGameEngine.h') === 0)
+                    out = out.replace(item.input, `<a href="https://github.com/OneLoneCoder/olcPixelGameEngine/blob/develop/olcPixelGameEngine.h#L${item.line}" target="_blank">${item.input}</a>`);
+            });
+            
+            elem_Information.innerHTML = out;
+            monaco_EditorDecorator = monaco_Editor.deltaDecorations([], entries);
+        })
+        .catch(function(error)
+        {
+            // act like we care, spoiler alert, we don't
+            console.log(error);
+        });
+    
+    }
 
-// save the state
-pgeLayout.on( 'stateChanged', function()
-{
-    var state = JSON.stringify( pgeLayout.toConfig() );
-    localStorage.setItem( 'pgeTinkerSavedLayout', state );
-});
+    function RefreshPlayer()
+    {
+        elem_PlayerStatus.className = 'loading';
+        
+        elem_Information.innerHTML = '';
+        elem_PlayerFrame.src = '/player';
+        setTimeout(function() { elem_PlayerStatus.className = ''; }, 1000);
+    }
 
-// initialize the layout
-pgeLayout.init();
+    function ResetLayout()
+    {
+        window.localStorage.removeItem('pgeTinkerSavedLayout');
+        window.location.reload();
+    }
+    
+    function Share()
+    {
+        axios.post('/api/share')
+        .then(function(response)
+        {
+            console.log(response.data);
+        })
+        .catch(function(error)
+        {
+            // act like we care, spoiler alert, we don't
+            console.log(error);
+        });
+    }
+
+    function GotoAndFocus(line, column)
+    {
+        monaco_Editor.setPosition({ lineNumber: line, column: column });
+        monaco_Editor.revealPositionInCenter({ lineNumber: line, column: column });
+        monaco_Editor.focus();        
+    }
+
+    // Expose API functions
+    return {
+        Compile: Compile,
+        GotoAndFocus: GotoAndFocus,
+        RefreshPlayer: RefreshPlayer,
+        ResetLayout: ResetLayout,
+        Share: Share,
+    }
+}();
+
+window.PGEtinker = PGEtinker;
