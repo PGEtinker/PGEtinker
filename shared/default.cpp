3f664c72699c5dc75e40553a2d25a22b554c1223
Moros1138 <moros1138@gmail.com>
2022-10-20 23:26:17 -0400

    a web relevant set of default code

--- shared/default.cpp
+++ shared/default.cpp
@@ -22,18 +22,22 @@ public:
     bool OnUserUpdate(float fElapsedTime) override
     {
         // Called once per frame, draws random coloured pixels
-        if(GetKey(olc::SPACE).bPressed)
+        if(GetMouse(0).bPressed)
+        {
             color = RandomColor();
+            std::cout << GetMousePos().str() << "\n";
+        }
+            
         
         Clear(color);
         DrawRect(0,0,ScreenWidth()-1, ScreenHeight()-1, olc::YELLOW);
         DrawString(6,  6, "Hello, PGE", olc::BLACK);
         DrawString(5,  5, "Hello, PGE", olc::WHITE);
-        DrawString(6, 26, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nSPACE to change color.", olc::BLACK);
-        DrawString(5, 25, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nSPACE to change color.", olc::WHITE);
+        DrawString(6, 26, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nLEFT MOUSE to change color.", olc::BLACK);
+        DrawString(5, 25, "Mouse position SHOULD match\nclosely to the circle.\n\nYellow borders should ALWAYS\nbe visible\n\nLEFT MOUSE to change color.", olc::WHITE);
         
-        DrawString(6, 221, GetWindowSize().str(), olc::BLACK);
-        DrawString(5, 220, GetWindowSize().str(), olc::WHITE);
+        DrawString(6, 221, GetMousePos().str(), olc::BLACK);
+        DrawString(5, 220, GetMousePos().str(), olc::WHITE);
         FillCircle(GetMousePos(), 3, olc::RED);
         Draw(GetMousePos(), olc::WHITE);
         return true;
