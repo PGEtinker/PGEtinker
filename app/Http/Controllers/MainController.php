375cc7571ca0db4c3f456f85aae457da14257e20
Moros1138 <moros1138@gmail.com>
2022-10-22 07:56:46 -0400

    stop a not found null access

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,21 +25,23 @@ class MainController extends Controller
     {
         $codeSlug = ltrim($request->getRequestUri(), '/');
 
-        $code = Code::where('slug', $codeSlug)->first();
-        
-        if($code)
+        $codeFromDB = Code::where('slug', $codeSlug)->first();
+        $code = '';
+
+        if($codeFromDB)
         {
             $base_data_path = base_path() . '/public/data/' . Session::get('pgeTinkerFilename');
 
             if(file_exists("{$base_data_path}.js"))   unlink("{$base_data_path}.js");
             if(file_exists("{$base_data_path}.wasm")) unlink("{$base_data_path}.wasm");
 
-            file_put_contents(base_path() . '/public/data/'. Session::get('pgeTinkerFilename') . '.cpp', $code->text);
+            file_put_contents(base_path() . '/public/data/'. Session::get('pgeTinkerFilename') . '.cpp', $codeFromDB->text);
+            $code = $codeFromDB->text;
         }
         
         return view('app', [
             'pgeTinkerFilename' => Session::get('pgeTinkerFilename'),
-            'code'              => $code->text,
+            'code'              => $code,
         ]);
     }
 
