b9ea48851dda5fa6f8165b1278c563d8b9c4732d
Moros1138 <moros1138@gmail.com>
2022-10-31 22:09:04 -0400

    embed meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,37 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>PGEtinker</title>
+        <meta name="description" value="Interactively develop an olc::PixelGameEngine program from your Browser!">
+
+        <meta property="og:title" content="PGEtinker">
+        <meta property="og:description" content="Interactively develop an olc::PixelGameEngine program from your Browser!">
+        <meta property="og:type" content="website">
+        
+        
+        @if($slug != 'null')
+        
+        <meta property="og:url" content="{{ env('APP_URL') }}/s/{{ $slug }}">
+        
+        @else
+        
+        <meta property="og:url" content="{{ env('APP_URL') }}">
+
+        @endif
+
+        <meta property="og:image" content="{{ env('APP_URL') }}{{ mix('images/PGEtinker-screenshot.png') }}">
+        <meta property="og:image:width" content="512">
+        <meta property="og:image:height" content="480">
+        <meta property="og:image:type" content="image/png">
+
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:description" content="Interactively develop an olc::PixelGameEngine program from your Browser!">
+        <meta property="twitter:title" content="PGEtinker">
+        <meta property="twitter:domain" content="{{ env('APP_URL') }}">        
+        <meta property="twitter:url" content="">
+        
+        <meta property="twitter:image" content="{{ env('APP_URL') }}{{ mix('images/PGEtinker-screenshot.png') }}">
+        <meta property="twitter:image:width" content="512">
+        <meta property="twitter:image:height" content="480">
 
         <script type="text/javascript" src="{{ mix('js/jquery.min.js') }}"></script>
         
