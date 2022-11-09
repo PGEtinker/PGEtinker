fbd9982ade341bfe5c99288fcbe3b5e4632f3ac7
Moros1138 <moros1138@gmail.com>
2022-11-09 00:01:20 -0500

    add timeout/killer

--- build-script.sh
+++ build-script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # attempt to compile
-docker exec pgetinker-builder em++ "$1.cpp" -o "$1.js" -I/src/third-party/olcPixelGameEngine -I/src/third-party/olcPixelGameEngine/extensions -I/src/third-party/olcPixelGameEngine/utilities -I/src/third-party/olcSoundWaveEngine -sASYNCIFY -sALLOW_MEMORY_GROWTH=1 -sMAX_WEBGL_VERSION=2 -sMIN_WEBGL_VERSION=2 -sUSE_LIBPNG=1 -sUSE_SDL_MIXER=2 -sLLD_REPORT_UNDEFINED
+timeout 10 docker exec pgetinker-builder em++ "$1.cpp" -o "$1.js" -I/src/third-party/olcPixelGameEngine -I/src/third-party/olcPixelGameEngine/extensions -I/src/third-party/olcPixelGameEngine/utilities -I/src/third-party/olcSoundWaveEngine -sASYNCIFY -sALLOW_MEMORY_GROWTH=1 -sMAX_WEBGL_VERSION=2 -sMIN_WEBGL_VERSION=2 -sUSE_LIBPNG=1 -sUSE_SDL_MIXER=2 -sLLD_REPORT_UNDEFINED
