3127d8f73cfdde78df8425e66ff7fbbb9d4b228b
Moros1138 <moros1138@gmail.com>
2022-11-26 00:54:09 -0500

    move docker related files into 'docker' folder

--- create-builder.sh
+++
@@ -1,3 +0,0 @@
-
-docker build -t pgetinker-builder .
-docker run -d --name pgetinker-builder -v "$(pwd)/public/data":/src/data -v "$(pwd)/third-party":/src/third-party -u "$(id -u)":"$(id -g)" -it pgetinker-builder:latest
